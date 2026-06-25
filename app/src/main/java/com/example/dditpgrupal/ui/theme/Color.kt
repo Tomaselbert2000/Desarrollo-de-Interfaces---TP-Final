af113ebc37e477aac378977690b994c79566508a
Tomaselbert2000 <tomas@debian.tomas>
2026-06-25 11:37:16 -0300

    feat: define paleta de colores del proyecto

--- app/src/main/java/com/example/dditpgrupal/ui/theme/Color.kt
+++ app/src/main/java/com/example/dditpgrupal/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.example.dditpgrupal.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryLight = Color(0xFF206A4E)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val SecondaryLight = Color(0xFF4D6357)
+val OnSecondaryLight = Color(0xFFFFFFFF)
+val BackgroundLight = Color(0xFFF5FBF5)
+val SurfaceLight = Color(0xFFF5FBF5)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryDark = Color(0xFFA8F2CE)
+val OnPrimaryDark = Color(0xFF003825)
+val SecondaryDark = Color(0xFFB3CCBD)
+val OnSecondaryDark = Color(0xFF1E352A)
+val BackgroundDark = Color(0xFF171D1A)
+val SurfaceDark = Color(0xFF171D1A)
